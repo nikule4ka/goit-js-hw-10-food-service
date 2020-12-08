da08220db36cbf36f312e3df5edeffebb7590964
Nika <yavtushenkonika@gmail.com>
2020-12-09 00:25:39 +0200

    adds menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+import itemMenu from './templates/menu.hbs';
+import menu from './menu.json';
 import './styles.css';
+// import './js/menu';
+
+
+const Menumarkup = itemMenu(menu);
+// console.log(Menumarkup);
+
+const menuRef = document.querySelector('.js-menu');
+
+// console.log(menuRef);
+
+menuRef.insertAdjacentHTML('afterbegin', Menumarkup);
